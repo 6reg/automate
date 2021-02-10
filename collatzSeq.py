704291ad7af9d931af8b34771883d0b6b039115f
greg mathias <mathiasgreg@gmail.com>
2021-02-09 19:03:07 -0500

    Working on adding error handling

--- collatzSeq.py
+++ collatzSeq.py
@@ -1,7 +1,10 @@
 def collatz(number):
     if number % 2 == 0:
-        print(number // 2)
-        return number // 2
+        try:
+            print(number // 2)
+            return number // 2
+        except TypeError: 
+            print("Error")
     number = 3 * number + 1
     print(number)
     return number
