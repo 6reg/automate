16d1ee58c912ce102894f4733fae05a8b8b9a4a6
Greg Mathias <mathiasgreg@gmail.com>
2021-02-13 18:57:27 -0500

    Collatz w exception handler

--- collatz.py
+++ collatz.py
@@ -1,8 +1,19 @@
 def collatz(number):
-    if number % 2 == 0:
-       return print(number // 2)
-    return print(3 * number + 1)
+        if number % 2 == 0:
+                number = number // 2
+                print(number)
+                return number
+        number = 3 * number + 1
+        print(number)
+        return number
 
-collatz(5)
-collatz(6)
+while True:
+    try:
+        number = int(input("enter #: "))
+    except ValueError:
+        print("Enter an int")
+    else: 
+        break
 
+while number != 1:
+    number = collatz(number)
